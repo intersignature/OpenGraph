b89220de9c7ec0fe320860b87c1062fe89954428
Drink Sirichai <intersignature@hotmail.com>
2025-11-04 19:56:32 +0700

    Support thai value

--- Sources/OpenGraph/OpenGraphParser.swift
+++ Sources/OpenGraph/OpenGraphParser.swift
@@ -6,6 +6,8 @@ public protocol OpenGraphParser {
 
 extension OpenGraphParser {
     func parse(htmlString: String) -> [OpenGraphMetadata: String] {
+        let nsString = htmlString as NSString
+
         // extract meta tag
         let metatagRegex  = try! NSRegularExpression(
             pattern: "<meta(?:\".*?\"|\'.*?\'|[^\'\"])*?>",
@@ -13,7 +15,7 @@ extension OpenGraphParser {
         )
         let metaTagMatches = metatagRegex.matches(in: htmlString,
                                        options: [],
-                                       range: NSMakeRange(0, htmlString.count))
+                                       range: NSMakeRange(0, nsString.length))
         if metaTagMatches.isEmpty {
             return [:]
         }
@@ -27,30 +29,29 @@ extension OpenGraphParser {
             pattern: "\\scontent=\\\\*?\"(.*?)\\\\*?\"",
             options: []
         )
-        
+
         // create attribute dictionary
-        let nsString = htmlString as NSString
         let attributes = metaTagMatches.reduce([OpenGraphMetadata: String]()) { (attributes, result) -> [OpenGraphMetadata: String] in
             var copiedAttributes = attributes
-            
+
             let property = { () -> (name: String, content: String)? in
-                let metaTag = nsString.substring(with: result.range(at: 0))
+                let metaTag = nsString.substring(with: result.range)
+                let nsMetaTag = metaTag as NSString
                 let propertyMatches = propertyRegexp.matches(in: metaTag,
                                                options: [],
-                                               range: NSMakeRange(0, metaTag.count))
+                                               range: NSMakeRange(0, nsMetaTag.length))
                 guard let propertyResult = propertyMatches.first else { return nil }
                 
-                var contentMatches = contentRegexp.matches(in: metaTag, options: [], range: NSMakeRange(0, metaTag.count))
+                var contentMatches = contentRegexp.matches(in: metaTag, options: [], range: NSMakeRange(0, nsMetaTag.length))
                 if contentMatches.first == nil {
                     let contentRegexp = try! NSRegularExpression(
                         pattern: "\\scontent=\\\\*?'(.*?)\\\\*?'",
                         options: []
                     )
-                    contentMatches = contentRegexp.matches(in: metaTag, options: [], range: NSMakeRange(0, metaTag.count))
+                    contentMatches = contentRegexp.matches(in: metaTag, options: [], range: NSMakeRange(0, nsMetaTag.length))
                 }
                 guard let contentResult = contentMatches.first else { return nil }
-                
-                let nsMetaTag = metaTag as NSString
+
                 let property = nsMetaTag.substring(with: propertyResult.range(at: 1))
                 let content = nsMetaTag.substring(with: contentResult.range(at: 1)).stringByDecodingHTMLEntities
                 
